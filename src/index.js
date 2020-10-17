4ce4ab018a4ce3750c6dcaa9bef35ccf17d9e294
Данила Семашко <59087699+danilasemashko@users.noreply.github.com>
2020-10-18 01:07:35 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,74 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr = [];
+    let arr2 = [];
+    let kol = 0;
+    let i = 0;
+let code =[]
+     arr = expr.match(/.{1,10}/g);
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === '**********') continue
+        arr[i] = +arr[i];
+        arr[i]= arr[i].toString()
+    }
+
+
+     for(i=0; i<arr.length; i++){
+     		if (arr[i] === '**********'){
+     			code.push('+')
+     		} else{
+     			arr2 = arr[i].match(/.{1,2}/g)
+     			for(let j = 0;j<arr2.length; j++){
+     				if (arr2[j] === '10'){
+     					code.push('.');
+     				}
+     				if (arr2[j] === '11'){
+     					code.push('-');
+     				}
+     			}
+     		}
+
+
+     		code.push(' ')
+     	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+code = code.join('').split(' ');
+
+      for(var key in MORSE_TABLE){
+        for(let c = 0; c< code.length; c ++){
+          if(code[c] === key){
+            code[c] = MORSE_TABLE[key]
+          } else if(code[c] === '+'){
+            code[c] = ' ';
+          }
+        }
+      }
+      
+      return code.join('');
+  }
 
 module.exports = {
     decode
